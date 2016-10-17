4a31f8372a0e4b59f8b13b91369e74c71b814c2e
shrkey <barry@shrkey.com>
2016-10-17 16:28:11 +0100

    updated servo example code

--- examples/Motor/Motor.cpp
+++ examples/Motor/Motor.cpp
@@ -1,16 +1,30 @@
 /*
-Provided to you by Emlid Ltd (c) 2014.
-twitter.com/emlidtech || www.emlid.com || info@emlid.com
+Example code is placed under the BSD license.
+Written by Team Dark Water (team@darkwater.io)
+Copyright (c) 2014, Dark Water
+All rights reserved.
 
-Example: Read accelerometer, gyroscope and magnetometer values from
-MPU9250 inertial measurement unit over SPI on Raspberry Pi + Navio.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Emlid Limited nor the names of its contributors
+      may be used to endorse or promote products derived from this software
+      without specific prior written permission.
 
-Navio's onboard MPU9250 is connected to the SPI bus on Raspberry Pi
-and can be read through /dev/spidev0.1
-
-To run this example navigate to the directory containing it and run following commands:
-make
-./AccelGyroMag
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL EMLID LIMITED BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include "darkwater/DWESCAPE.h"
